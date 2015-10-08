2d206cef4aa445b0e89179b4f1a418c645d42269
Karl <karlmolden@hotmail.com>
2015-10-08 09:52:40 +0100

    Create script.R
    First version of main script for project data analysis

---
+++ script.R
@@ -0,0 +1,7 @@
+### This file contains the code needed to run the analysis for the JISC Micro-project ###	
+### "Can survey data be used as an indicator of learning outcomes?"                   ###
+### Version 1.0                                                                       ###
+### 07/10/2015                                                                        ###
+### Karl Molden - Planning & Statistics - University of Greenwich                     ###
+ 
+setwd("U:/PAS/JISC/Analytics Micro Project/Data")
